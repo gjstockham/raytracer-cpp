132ab1b99f1d1279cbd480e4b0a3d220edb3b3d8
Geoff Stockham <geoff@stormid.com>
2016-02-26 11:47:55 +0000

    ch5: add hitable classes

--- raytracer-cpp/vec3.h
+++ raytracer-cpp/vec3.h
@@ -71,9 +71,13 @@ public:
 	}
 
 	inline vec3 cross(const vec3 &v1, const vec3 &v2) {
-		return vec3((v1.e[1] * v2.e[2] - v2.e[2] * v2.e[1]),
+		return vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
 			(-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
 			(v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
+	}
+
+	inline vec3 unit_vector(const vec3 &v1) {
+		return v1 / v1.length();
 	}
 
 #endif
